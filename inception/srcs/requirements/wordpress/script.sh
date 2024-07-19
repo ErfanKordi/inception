0d0ea0dafbd27d04de7084dde6247aea91578836
Erfankd <erfan.12.kordi@gmail.com>
2024-07-19 15:28:49 +0200

    wp script fix

--- inception/srcs/requirements/wordpress/script.sh
+++ inception/srcs/requirements/wordpress/script.sh
@@ -24,11 +24,11 @@ chmod 777 wp-config.php
 # Install WordPress with environment variables
 ./wp-cli.phar core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_SU_USR --admin_password=$WP_SU_PWD --admin_email=$WP_SU_EMAIL --allow-root
 
-wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
-wp option update home $DOMAIN_NAME --allow-root
-wp option update siteurl  $DOMAIN_NAME --allow-root
-# chown -R www-data:www-data /var/www/html/wp-content/uploads
-# chmod -R 755 /var/www/html/wp-content/uploads
+./wp-cli.phar user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
+./wp-cli.phar option update home $DOMAIN_NAME --allow-root
+./wp-cli.phar option update siteurl  $DOMAIN_NAME --allow-root
+chown -R www-data:www-data /var/www/html/wp-content/uploads
+chmod -R 755 /var/www/html/wp-content/uploads
 # WP_PATH="/var/www/html"
 
 # # Change ownership to www-data (adjust if necessary for your web server user)
