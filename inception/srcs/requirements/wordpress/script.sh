3cd55269568d883df9332e3feef5050bd477ddbc
Erfankd <erfan.12.kordi@gmail.com>
2024-07-19 15:47:15 +0200

    wp script fix

--- inception/srcs/requirements/wordpress/script.sh
+++ inception/srcs/requirements/wordpress/script.sh
@@ -24,21 +24,16 @@ chmod 777 wp-config.php
 # Install WordPress with environment variables
 ./wp-cli.phar core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_SU_USR --admin_password=$WP_SU_PWD --admin_email=$WP_SU_EMAIL --allow-root
 
+# Create a new user
 ./wp-cli.phar user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
+
+# Update WordPress options
 ./wp-cli.phar option update home $DOMAIN_NAME --allow-root
-./wp-cli.phar option update siteurl  $DOMAIN_NAME --allow-root
+./wp-cli.phar option update siteurl $DOMAIN_NAME --allow-root
+
+# Ensure proper ownership and permissions for uploads directory
 chown -R www-data:www-data /var/www/html/wp-content/uploads
 chmod -R 755 /var/www/html/wp-content/uploads
-# WP_PATH="/var/www/html"
-
-# # Change ownership to www-data (adjust if necessary for your web server user)
-# sudo chown -R www-data:www-data $WP_PATH/wp-content/uploads
-
-# # Set directory permissions to 755
-# sudo find $WP_PATH/wp-content/uploads -type d -exec chmod 755 {} \;
-
-# # Set file permissions to 644
-# sudo find $WP_PATH/wp-content/uploads -type f -exec chmod 644 {} \;
 
 # Start PHP-FPM
 php-fpm8.2 -F
